5c3af9e4c6a45c619f4da5e8897ff604245fbd2c
rmatcas03 <rmatcas03@iesmarquesdecomares.org>
2021-11-11 12:33:26 +0100

    commit 11/11

---
+++ framework-php-bootstrap/plantilla_index.php
@@ -0,0 +1,22 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<main>
+<div class="row justify-content-center">
+        <h2>PLantilla layout</h2>
+
+    </div>
+</main>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
