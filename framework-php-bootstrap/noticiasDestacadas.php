fea70f71e0e507f2ad6b8e8ffdc3800bf7ce0f46
David142000 <dgargar01@iesmarquesdecomares.org>
2021-11-18 11:18:11 +0100

    noticias destacadas terminado

--- framework-php-bootstrap/noticiasDestacadas.php
+++ framework-php-bootstrap/noticiasDestacadas.php
@@ -18,33 +18,66 @@
       </div>
       <div class="container-fluid row mt-3">
 
-        <div class="card col-4">
+        <div class="card col-4 border-0">
           <h2 class="text-center">Fútbol base</h2>
-          <img class="card-img-top" src="imgTrabajo\fb01.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="imgTrabajo\fff.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
-            <a href="#" class="$secondary">Más información</a>
+            <a href="#" class="btn btn-secondary">Más información</a>
           </div>
         </div>
 
-        <div class="card col-4">
-          <h2 class="text-center">Baloncesto</h2>
-          <img class="card-img-top" src="imgTrabajo\fb01.jpg" alt="Card image" style="width:100%">
+        <div class="card col-4 border-0">
+          <h2 class="text-center">Natación</h2>
+          <img class="card-img-top" src="imgTrabajo\natacionhd.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
-            <a href="#" class="$primary">Más información</a>
+            <a href="#" class="btn btn-secondary">Más información</a>
           </div>
         </div>
 
-        <div class="card col-4">
+        <div class="card col-4 border-0">
           <h2 class="text-center">Pádel</h2>
-          <img class="card-img-top" src="imgTrabajo\fb01.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="imgTrabajo\padel.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
-            <a href="#" class="$primary">Más información</a>
+            <a href="#" class="btn btn-secondary">Más información</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid row my-5">
+
+        <div class="card col-4 border-0">
+          <h2 class="text-center">Defensa personal</h2>
+          <img class="card-img-top" src="imgTrabajo\defensa personalhd.jpg" alt="Card image" style="width:100%">
+          <div class="card-body">
+            <h4 class="card-title">Lucena fc</h4>
+            <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
+            <a href="#" class="btn btn-secondary">Más información</a>
+          </div>
+        </div>
+
+        <div class="card col-4 border-0">
+          <h2 class="text-center">Atletismo</h2>
+          <img class="card-img-top" src="imgTrabajo\atletismo01.jpg" alt="Card image" style="width:100%">
+          <div class="card-body">
+            <h4 class="card-title">Lucena fc</h4>
+            <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
+            <a href="#" class="btn btn-secondary">Más información</a>
+          </div>
+        </div>
+
+        <div class="card col-4 border-0">
+          <h2 class="text-center">Tenis</h2>
+          <img class="card-img-top" src="imgTrabajo\tenis.jpg" alt="Card image" style="width:100%">
+          <div class="card-body">
+            <h4 class="card-title">Lucena fc</h4>
+            <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
+            <a href="#" class="btn btn-secondary">Más información</a>
           </div>
         </div>
       </div>
