6d3a763591cfe43ef81e67d71df739b6d945817f
rmatcas03 <rmatcas03@iesmarquesdecomares.org>
2021-11-18 11:20:55 +0100

    img

--- framework-php-bootstrap/noticiasDestacadas.php
+++ framework-php-bootstrap/noticiasDestacadas.php
@@ -20,7 +20,7 @@
 
         <div class="card col-4 border-0">
           <h2 class="text-center">Fútbol base</h2>
-          <img class="card-img-top" src="imgTrabajo\fff.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="assets\img\imgTrabajo\fff.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
@@ -30,7 +30,7 @@
 
         <div class="card col-4 border-0">
           <h2 class="text-center">Natación</h2>
-          <img class="card-img-top" src="imgTrabajo\natacionhd.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="assets\img\imgTrabajo\natacionhd.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
@@ -40,7 +40,7 @@
 
         <div class="card col-4 border-0">
           <h2 class="text-center">Pádel</h2>
-          <img class="card-img-top" src="imgTrabajo\padel.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="assets\img\imgTrabajo\padel.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
@@ -53,7 +53,7 @@
 
         <div class="card col-4 border-0">
           <h2 class="text-center">Defensa personal</h2>
-          <img class="card-img-top" src="imgTrabajo\defensa personalhd.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="assets\img\imgTrabajo\defensa personalhd.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
@@ -63,7 +63,7 @@
 
         <div class="card col-4 border-0">
           <h2 class="text-center">Atletismo</h2>
-          <img class="card-img-top" src="imgTrabajo\atletismo01.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="assets\img\imgTrabajo\atletismo01.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
@@ -73,7 +73,7 @@
 
         <div class="card col-4 border-0">
           <h2 class="text-center">Tenis</h2>
-          <img class="card-img-top" src="imgTrabajo\tenis.jpg" alt="Card image" style="width:100%">
+          <img class="card-img-top" src="assets\img\imgTrabajo\tenis.jpg" alt="Card image" style="width:100%">
           <div class="card-body">
             <h4 class="card-title">Lucena fc</h4>
             <p class="card-text">Comienzan las clases de fútbol en nuestra academia municipal</p>
