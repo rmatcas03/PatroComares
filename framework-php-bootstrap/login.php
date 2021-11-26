b0827395d703a48ca16d18a785f8995f9624656f
David142000 <dgargar01@iesmarquesdecomares.org>
2021-11-26 13:50:44 +0100

    LOGIN ACABADO

--- framework-php-bootstrap/login.php
+++ framework-php-bootstrap/login.php
@@ -17,27 +17,42 @@
                     <div class="col-lg-6">
                         <div class="row-12 my-3">
                             <div class="col">
-                                <h2>Mi perfil</h1>
+                            <h2 class="mx-5">Mi perfil</h1>
+                                <div class="container-fluid text-center">
+                                   
+
+                                        <div class="card border-0" style="width:250px">
+                                            <img class="card-img-top " src="assets\img\imgTrabajo\image.jpg" alt="Card image">
+                                            <div class="card-body">
+
+
+                                                <a href="#" class="btn btn-secondary">Cambiar</a>
+                                            </div>
+                                        </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="row-12 my-3">
-                            <div class="col">
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="row-12 my-3 ">
+                            <div class="col  d-flex justify-content-end">
+                                <!-- d-flex justify-content-end te pone los componentes a la derecha-->
                                 <form>
                                     <div class="form-group my-3">
                                         <label for="exampleFormControlInput1">Nombre</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="nombre">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre">
                                     </div>
                                     <div class="form-group my-3">
                                         <label for="exampleFormControlInput1">Apellido1</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="apellido1">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Apellido1">
                                     </div>
                                     <div class="form-group my-3">
                                         <label for="exampleFormControlInput1">Apellido2</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="apellido2">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Apellido2">
                                     </div>
                                     <div class="form-group my-3">
                                         <label for="exampleFormControlInput1">Teléfono</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="telefono">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Telefono">
                                     </div>
                                     <div class="form-group my-3">
                                         <label for="exampleFormControlInput1">Correo electrónico</label>
@@ -45,19 +60,20 @@
                                     </div>
                                     <div class="form-group my-3">
                                         <label for="exampleFormControlInput1">Dirección</label>
-                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="direccion">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Direccion">
                                     </div>
                                     <div class="form-group my-3">
                                         <label for="exampleFormControlInput1">Password</label>
-                                        <input type="text" class="password" id="exampleFormControlInput1" placeholder="password">
-                                    
+                                        <input type="password" class="password" id="exampleFormControlInput1" placeholder="Password">
+
                                         <button type="button" class="btn btn-secondary">Cambiar</button>
                                     </div>
+
                             </div>
                         </div>
 
                     </div>
-                  
+
                 </div>
         </section>
     </main>
