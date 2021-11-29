01ede2bddbcfbdaef0c098d4aeed4d9efa729a26
joselu12345 <joselu.ruzgil@gmail.com>
2021-11-29 11:27:59 +0100

    kjh

--- framework-php-bootstrap/actividadesFutbol.php
+++ framework-php-bootstrap/actividadesFutbol.php
@@ -25,7 +25,7 @@
               
               <div class="card-body">
                 <p class="card-text"> Liga de futbol infantil </p>
-                <img class="card-img-top" src="emoji.jpg" alt="Card image cap">
+                <img class="card-img-top" src="assets\img\imgActividades\futbol.jpg" alt="Card image cap">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.
 
                                     Some quick example text to build on the card title and make up the bulk of the card's content.
