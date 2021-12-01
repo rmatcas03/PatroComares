bea5f02fc6ad198bdd4c0a87dfe158cf115c0cd1
jesusbomoriles2016 <72796727+jesusbomoriles2016@users.noreply.github.com>
2021-12-01 13:17:37 +0100

    terminado

--- framework-php-bootstrap/instalacionesAtletismo.php
+++ framework-php-bootstrap/instalacionesAtletismo.php
@@ -15,10 +15,10 @@
 
             <div class="d-flex justify-content-between row my-3">
                 <div class="my-3">
-                    <h2>Instalaciones Baloncesto</h2>
+                    <h2>Instalaciones Atletismo</h2>
                 </div>
                 <div class="col-6">
-                    <img class="img-fluid" src="assets\img\imgTrabajo\baloncesto.png">
+                    <img class="img-fluid" src="assets\img\imgTrabajo\atletismo.png">
                 </div>
                 <div class="col-6 d-flex flex-column">
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
@@ -36,7 +36,7 @@
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
                 <div class="col-6 d-flex flex-column">
-                    <img class="img-fluid" src="assets\img\imgTrabajo\baloncesto.png">
+                    <img class="img-fluid" src="assets\img\imgTrabajo\atletismo.png">
                 </div>
 
             </div>
