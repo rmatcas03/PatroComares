f108a9db40271e689de47897e589f63c33ec31b2
joselu12345 <joselu.ruzgil@gmail.com>
2021-12-03 12:17:57 +0100

    commit

---
+++ framework-php-bootstrap/quienesSomos.php
@@ -0,0 +1,92 @@
+<?php include("includes/a_config.php"); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include("includes/head-tag-contents.php"); ?>
+</head>
+
+<body>
+
+    <?php include("includes/navigation.php"); ?>
+    <main>
+        
+        <section class="page-section" id="">
+            <div class="container-fluid">
+
+            <div class="text-center mt-3">
+            <h1> ¿Quienes Somos?</h1>
+        </div>
+        
+        <div class="page-section" id="actividades">
+
+             <div class="row ">
+
+                <div class="col-lg-6">
+
+                    <div class="row border border-5 border-info m-1">
+                        <div class="col">
+                        <img class="img-fluid p-1" src="assets\img\imgActividades\emoji2.jpg" alt="Card image">
+                        </div>
+                        <div class="col">
+                            <h3>Datos Personales</h3>
+                            <p>Nombre:</p>
+                            <p>Apellidos:</p>
+                            <p>Direccion:</p>
+                            <p>Telefono:</p>
+                        </div>
+                    </div>  
+                    <div class="row border border-5 border-info m-1 mb-5">
+                        <div class="col">
+                        <img class="img-fluid p-1" src="assets\img\imgActividades\emoji2.jpg" alt="Card image">
+                        </div>
+                        <div class="col">
+                            <h3>Datos Personales</h3>
+                            <p>Nombre:</p>
+                            <p>Apellidos:</p>
+                            <p>Direccion:</p>
+                            <p>Telefono:</p>
+                        </div>
+                    </div>  
+                 
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="row border border-5 border-info m-1">
+                        <div class="col">
+                        <img class="img-fluid p-1" src="assets\img\imgActividades\emoji2.jpg" alt="Card image">
+                        </div>
+                        <div class="col">
+                            <h3>Datos Personales</h3>
+                            <p>Nombre:</p>
+                            <p>Apellidos:</p>
+                            <p>Direccion:</p>
+                            <p>Telefono:</p>
+                        </div>
+                    </div>  
+                    <div class="row border border-5 border-info m-1 mb-5">
+                        <div class="col">
+                        <img class="img-fluid p-1" src="assets\img\imgActividades\emoji2.jpg" alt="Card image">
+                        </div>
+                        <div class="col">
+                            <h3>Datos Personales</h3>
+                            <p>Nombre:</p>
+                            <p>Apellidos:</p>
+                            <p>Direccion:</p>
+                            <p>Telefono:</p>
+                        </div>
+                    </div>  
+                
+                </div>
+             
+         </div>
+        </div>
+
+            </div>  
+        </section>
+    </main>
+    <?php include("includes/footer.php"); ?>
+
+</body>
+
+</html>
