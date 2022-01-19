8b5eeab5cfd3c5938dbaaa7d1ce03ab027946814
rmatcas03 <rmatcas03@iesmarquesdecomares.org>
2022-01-19 12:42:14 +0100

    termimos

--- framework-php-bootstrap/terminos.php
+++ framework-php-bootstrap/terminos.php
@@ -12,68 +12,71 @@
 
     <main>
 
-        <div class="row justify-content-center">
-            <div class="container p-4">
-                <h2>Aviso Legal </h2>
-                <span>
-                    Tanto el sitio Web www.patrocomares.es como los restantes subdominios de patrocomares.es
-                    pertenecen al Ayuntamiento de Lucena, Avenida Juego de pelota (Instalaciones Deportivas Municipales Lucena),
-                    s/n. 23001.<br>
-                    Puede contactar con el Ayuntamiento a través del teléfono 953 239 126. <br>
-                    Todos los servicios contenidos en este sitio Web son prestados por el Ayuntamiento conforme a la legislación
-                    y normativa vigentes.
-                </span>
-                <h2>Política de seguridad de la información</h2>
-                <p>
-                    Como aplicación del Real Decreto 3/2010 por el que se regula el Esquema Nacional de Seguridad en el ámbito
-                    de la Administración Electrónica,
-                    el Ayuntamiento de Lucena dispone de una Política de Seguridad de la Información, como norma base para la
-                    gestión y aseguramiento de la información manejada, incluida la referente a datos de carácter personal,
-                    en los sistemas de información de los que dispone.
-                </p>
+        <div class="container-fluid p-3">
+            <h2>Aviso Legal </h2>
+            <span>
+                Tanto el sitio Web www.patrocomares.es como los restantes subdominios de patrocomares.es
+                pertenecen al Ayuntamiento de Lucena, Avenida Juego de pelota (Instalaciones Deportivas Municipales Lucena),
+                s/n. 23001.<br>
+                Puede contactar con el Ayuntamiento a través del teléfono 953 239 126. <br>
+                Todos los servicios contenidos en este sitio Web son prestados por el Ayuntamiento conforme a la legislación
+                y normativa vigentes.
+            </span>
+            <h2>Política de seguridad de la información</h2>
+            <p>
+                Como aplicación del Real Decreto 3/2010 por el que se regula el Esquema Nacional de Seguridad en el ámbito
+                de la Administración Electrónica,
+                el Ayuntamiento de Lucena dispone de una Política de Seguridad de la Información, como norma base para la
+                gestión y aseguramiento de la información manejada, incluida la referente a datos de carácter personal,
+                en los sistemas de información de los que dispone.
+            </p>
 
-                <h2>Propiedad intelectual</h2>
-                <p>
-                    Todos los textos y contenidos multimedia publicados en este sitio Web son titularidad del Ayuntamiento,
-                    salvo que se indique lo contrario. Queda prohibida su reproducción, distribución, comunicación pública y
-                    transformación, salvo para uso personal y privado. Si desea reutilizar nuestros contenidos de manera habitual
-                    para uso público, debe contar con una autorización expresa del Ayuntamiento, debiendo hacer constar su procedencia.
-                </p>
-                <h2>Seguridad de los datos personales</h2>
-                <p>
-                    Para proteger tus datos personales, el Titular toma todas las precauciones razonables y sigue las mejores
-                    prácticas de la industria para evitar su pérdida, mal uso, acceso indebido, divulgación, alteración o
-                    destrucción de los mismos.
+            <h2>Propiedad intelectual</h2>
+            <p>
+                Todos los textos y contenidos multimedia publicados en este sitio Web son titularidad del Ayuntamiento,
+                salvo que se indique lo contrario. Queda prohibida su reproducción, distribución, comunicación pública y
+                transformación, salvo para uso personal y privado. Si desea reutilizar nuestros contenidos de manera habitual
+                para uso público, debe contar con una autorización expresa del Ayuntamiento, debiendo hacer constar su procedencia.
+            </p>
+            <h2>Seguridad de los datos personales</h2>
+            <p>
+                Para proteger tus datos personales, el Titular toma todas las precauciones razonables y sigue las mejores
+                prácticas de la industria para evitar su pérdida, mal uso, acceso indebido, divulgación, alteración o
+                destrucción de los mismos.
 
-                    Tus datos podrán ser incorporados a un fichero de lista de correo, del cual Patronato Municipal de Deportes
-                    de Lucena toma todas las medidas de seguridad necesarias y te garantiza que los datos personales sólo se
-                    usarán para las finalidades dadas.
+                Tus datos podrán ser incorporados a un fichero de lista de correo, del cual Patronato Municipal de Deportes
+                de Lucena toma todas las medidas de seguridad necesarias y te garantiza que los datos personales sólo se
+                usarán para las finalidades dadas.
 
-                    Patronato Municipal de Deportes de Lucena informa al Usuario de que sus datos personales no serán cedidos a
-                    terceras organizaciones, con la salvedad de que dicha cesión de datos esté amparada en una obligación legal
-                    o cuando la prestación de un servicio implique la necesidad de una relación contractual con un encargado
-                    de tratamiento. En este último caso, solo se llevará a cabo la cesión de datos al tercero cuando Patronato
-                    Municipal de Deportes de Lucena disponga del consentimiento expreso del Usuario.
+                Patronato Municipal de Deportes de Lucena informa al Usuario de que sus datos personales no serán cedidos a
+                terceras organizaciones, con la salvedad de que dicha cesión de datos esté amparada en una obligación legal
+                o cuando la prestación de un servicio implique la necesidad de una relación contractual con un encargado
+                de tratamiento. En este último caso, solo se llevará a cabo la cesión de datos al tercero cuando Patronato
+                Municipal de Deportes de Lucena disponga del consentimiento expreso del Usuario.
 
-                    Sin embargo, en algunos casos se pueden realizar colaboraciones con otros profesionales, en esos casos,
-                    se requerirá consentimiento al Usuario informando sobre la identidad del colaborador y la finalidad de la
-                    colaboración. Siempre se realizará con los más estrictos estándares de seguridad.
-                </p>
+                Sin embargo, en algunos casos se pueden realizar colaboraciones con otros profesionales, en esos casos,
+                se requerirá consentimiento al Usuario informando sobre la identidad del colaborador y la finalidad de la
+                colaboración. Siempre se realizará con los más estrictos estándares de seguridad.
+            </p>
 
-                <h2>Términos y licencias de las imágenes utilizadas</h2>
-                <p>Todas las imágenes han sido proporcionadas por la página <a href="https://www.flickr.com/"> flickr</a>.</p>
-                <ul>
-                    <li>Las imágenes utilizadas en la página "Actividades", licencia
-                        <a href="https://creativecommons.org/licenses/by-nc/2.0/"> aquí</a>.
-                    </li>
-                    <li>Las imágenes utilizadas en la página "Instalaciones", licencia
-                        <a href="https://creativecommons.org/licenses/by-sa/2.0/"> aquí</a>.
-                    </li>
-                    <li>Las imágenes utilizadas en la página "Noticias", licencia
-                        <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/"> aquí</a>.
-                    </li>
-                </ul>
-            </div>
+            <h2>Términos y licencias de las imágenes utilizadas</h2>
+            <p>Todas las imágenes han sido proporcionadas por la página <a href="https://www.flickr.com/"> flickr </a> y <a href="https://www.pexels.com/es-es/"> pexels</a>.</p>
+            <ul>
+                <li>Las imágenes utilizadas en la página "Actividades", licencia
+                    <a href="https://creativecommons.org/licenses/by-nc/2.0/"> aquí</a>.
+                </li>
+                <li>Las imágenes utilizadas en la página "Instalaciones", licencia
+                    <a href="https://creativecommons.org/licenses/by-sa/2.0/"> aquí</a>.
+                </li>
+                <li>Las imágenes utilizadas en la página "Noticias", licencia
+                    <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/"> aquí</a>.
+                </li>
+                <li>Las imágenes utilizadas en la página "Home", licencia
+                    <a href="https://www.pexels.com/es-es/creative-commons-images/"> aquí</a>.
+                </li>
+
+
+            </ul>
 
         </div>
     </main>
