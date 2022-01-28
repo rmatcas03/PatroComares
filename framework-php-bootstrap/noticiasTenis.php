9b5b914359ccba923e84c233932e6b388435dc3a
David142000 <dgargar01@iesmarquesdecomares.org>
2022-01-28 12:56:58 +0100

    login

--- framework-php-bootstrap/noticiasTenis.php
+++ framework-php-bootstrap/noticiasTenis.php
@@ -10,21 +10,24 @@
 
   <?php include("includes/navigation.php"); ?>
 
+  
   <main>
 
-    <section class="page-section" id="noticias">
+  <section class="page-section" id="noticias">
      
-      <div class="text-center mt-5 row">
-        <div class="col d-flex justify-content-start mx-5">
-          <a href="noticiasDestacadas.php" class="btn btn-secondary mx-5 py-2" style="height: 40px;"><<<</a>
-          <h1>Formación de nuestra académia</h1>
-        </div>
+     <div class="text-center mt-5 row">
+       <div class="col d-flex justify-content-start mx-5">
+         <a href="noticiasDestacadas.php" class="btn btn-secondary mx-5 py-2" style="height: 40px;"><<<</a>
+         <h1>Formación de nuestra académia</h1>
+       </div>
 
-      </div>
-      <div class="col-lg-8">
+     </div>
+      <div class=" container-fluid text-center">
+        <div class="row mt-3">
+          <div class="col-lg-8">
             <div class="card  border-0 polaroid">
               <h2 class="text-center">Tenis</h2>
-              <img class="card-img-top" src="assets\img\imgActividades\tenis12.jpg" alt="Card image" style="width:100%">
+              <img class="card-img-top" src="assets\img\imgTrabajo\tenis123.jpg" alt="Card image" style="width:100%">
               <div class="card-body">
                 <h4 class="card-title">Clases de tenis</h4>
               </div>
@@ -34,16 +37,11 @@
 
 
 
-
-
-
           Las inscripciones para la temporada 2022/2023 del Ranking Juvenil Federado Lucena van a comenzar. Dará comienzo el 1 de Febrero y las inscripciones están abiertas hasta el 26 de Marzo, a un precio de 20€ , para más información consulte nuestra web o llámanos.
 
           </div>
         </div>
       </div>
-
-
     </section>
 
   </main>
