cbeddf903329f7018fcc653ae8fc40f14b0692d0
jesusbomoriles2016 <72796727+jesusbomoriles2016@users.noreply.github.com>
2022-02-04 09:50:12 +0100

    Revert "Merge branch 'main' of https://github.com/rmatcas03/PatroComares into main"
    This reverts commit b5ebe744f4bd4b7161f257ea52bef7aec304b8ec, reversing
    changes made to 0aa64548b7491104b6c18b27d542dad38e5f2d59.

--- framework-php-bootstrap/includes/a_config.php
+++ framework-php-bootstrap/includes/a_config.php
@@ -64,21 +64,3 @@ switch ($_SERVER["SCRIPT_NAME"]) {
 		$CURRENT_PAGE = "Index";
 		$PAGE_TITLE = "Welcome to my homepage!";
 }
-
-require_once 'vendor/autoload.php';
-
-$google_client = new Google_Client();
-
-$google_client->setClientId('462756506190-7tn2ikihdfqru2nh4gs8j3veoe1novuv.apps.googleusercontent.com');
-
-$google_client->setClientSecret('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');
-
-$google_client->setRedirectUri('http://'.$_SERVER['SERVER_NAME'].'/index.php');
-
-$google_client->addScope('email');
-
-$google_client->addScope('profile');
-
-session_start();
-
-$login_button = '';
--- framework-php-bootstrap/register.php
+++ framework-php-bootstrap/register.php
@@ -11,70 +11,62 @@
     <?php include("includes/navigation.php"); ?>
     <main>
         <!-- Contact Section-->
-        <body>
+        <div class="container-fluid mb-5">
 
-        
+        <div class="d-flex justify-content-between row my-3 ">
 
-<div class="container-md">
-    <div class="row my-5 d-flex justify-content-center container-fluid"> <!-- contiene la tarjeta, por lo que ajusta la posición de la misma-->
-        
-        <div class="card col-md-8 border-0 ">
-            <div class="row">
-                <header class="card-header py-4 " style="width: 100%;">
-                    <h2 style="font-family: 'Roboto', sans-serif"  class="text-center">Únete a nosotros!</h2>
-                </header>
-            </div>
+<div class="col-lg-6">
+    <div class="card border-0 mx-5" style="width:250px">
+        <h2 class="mx-5 text-center">Mi perfil</h2>
+        <img class="card-img-top " src="assets\img\imgTrabajo\image.jpg" alt="Card image">
+        <div class="card-body">
 
-            <div class="card-body ">
-                
-                <div class="form-group row">
-                    <div class="col-md-6 form-group">
-                        <label for="nombre">Nombre:</label>
-                        <input type="nombre" class="form-control" id="nombre" placeholder="Nombre" name="nombre">
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label for="apellidos">Apellidos:</label>
-                        <input type="apellidos" class="form-control" id="apellidos" placeholder="Apellidos" name="apellidos">
-                    </div>
-                    
-                </div>
-
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="Email" name="email">
-                </div>
-
-                <div class="form-group row">
-                    <div class="col-md-6 form-group">
-                        <label for="contraseña">Contraseña:</label>
-                        <input type="contraseña" class="form-control" id="Contraseña" placeholder="Contraseña" name="contraseña">
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label for="confirContraseña">Confirmar contraseña:</label>
-                        <input type="confirContraseña" class="form-control" id="confirContraseña" placeholder="Confirmar Contraseña" name="confirContraseña">
-                    </div>
-                    
-                </div>
-                <hr>
-
-
-
-                <div class="d-flex ">
-                    <button type="submit" class="btn btn-secondary col-12 ">Registrar</button>
-                </div>
-            </div>
-            <div class="row">
-            <p><a  id="azulillo" href="login.php">¿Ya está registrado? ve a login!</a> </p>
+            <div class="container-fluid text-center">
+                <!--pone centrado el boton de perfil-->
+                <a href="#" class="btn btn-secondary">Cambiar</a>
             </div>
         </div>
     </div>
 </div>
+<div class="col-lg-6 d-flex flex-column text-start">
+    <form>
+        <div class="form-group my-3">
+            <label for="exampleFormControlInput1">Nombre</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nombre">
+        </div>
+        <div class="form-group my-3">
+            <label for="exampleFormControlInput1">Apellido1</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Apellido1">
+        </div>
+        <div class="form-group my-3">
+            <label for="exampleFormControlInput1">Apellido2</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Apellido2">
+        </div>
+        <div class="form-group my-3">
+            <label for="exampleFormControlInput1">Teléfono</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Telefono">
+        </div>
+        <div class="form-group my-3">
+            <label for="exampleFormControlInput1">Correo electrónico</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="form-group my-3">
+            <label for="exampleFormControlInput1">Dirección</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Direccion">
+        </div>
+        <div class="form-group my-3 d-flex flex-row align-items-center ">
+            <label for="exampleFormControlInput1">Password</label>
+            <input type="password" class="password flex-grow-1 mx-1" id="exampleFormControlInput1" placeholder="Password">
+        </div>  
+        
+        <div class="align-items-center text-center ">
+        <button type="button" class="btn btn-secondary">Confirmar</button>
+        </div>
+    </form>
+</div>
 
-
-
-
-
-</body>
+            </div>
+        </div>
     </main>
     <?php include("includes/footer.php"); ?>
 
